ba1b58daff25a0a149024e07bc0c38afed137e0d
BlancaCC <blancacano2.71828@gmail.com>
2021-10-31 09:34:15 +0100

    DONE setTimeat close #4 #6 #7 Comunicacion con el backend acabada (teniendo presente que hace falta modelar las cosas que va a hacer el lead

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,27 +2,36 @@ import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
 import { appStateUrl} from './routes'
-
+const TIMEOUT = 1000 // milisegundos en hacer petición (1000ms = 1s)
 function App() {
   const [state, setState ] = useState(null)
+  const [recarga, setRecarga] = useState(0)
   // cuando se monta 
 
   useEffect( ()=> {
-    axios.get(appStateUrl)
-    .then( res => {
-      setState(res.data)
-      console.log(res.data)
-      //return res.data
-    })
-    .catch( (err) => {
-      console.log(err)
-    })
+
+    const timer = setTimeout(() => {
+      axios.get(appStateUrl)
+      .then( res => {
+        setState(res.data)
+        console.log(res.data)
+        //return res.data
+        setRecarga(recarga+1)
+      })
+      .catch( (err) => {
+        console.log(err)
+      })
+  }, TIMEOUT);
+  return () => clearTimeout(timer)
   })
+
 
   return (
     <div className="App">
-      <h1> Hello world</h1>
-      El estado leído es: 
+      <h1> Página debugeo de state</h1>
+
+      Peticiones desde que se lanzó la página al backend :{recarga} <br/>
+      <h2> La información que contiene el estado </h2>
       View: {state?.view} <br/>
       Select: {state?.select}
     </div>
