f88f763087fc0accc1f0ca5ccda8462deaa22b40
BlancaCC <blancacano2.71828@gmail.com>
2021-10-31 08:42:44 +0100

    fronted] WIP #4 creating the structure for the petition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+import { appStateUrl} from './routes'
 
 function App() {
+  const [state, setState ] = useState(null)
+  // cuando se monta 
+
+  useEffect( ()=> {
+    axios.get(appStateUrl, 
+      {
+        headers:{
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+    .then( res => {
+      setState(res.data)
+      console.log(res.data)
+      //return res.data
+    })
+    .catch( (err) => {
+      console.log(err)
+    })
+  })
+
   return (
     <div className="App">
       <h1> Hello world</h1>
+      El estado leído es: 
+      {state}
     </div>
   );
 }
---
+++ frontend/src/routes.js
@@ -0,0 +1,1 @@
+export const appStateUrl = 'http://localhost:8000/'
