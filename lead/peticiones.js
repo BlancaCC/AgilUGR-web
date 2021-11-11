35da1e039bb3d717f13f866e602889c64c5e18d6
BlancaCC <blancacano2.71828@gmail.com>
2021-11-11 22:48:50 +0100

    #21 Funcion general para hacer put creada  a falta de añadir elpath que use #22

--- lead/peticiones.js
+++ lead/peticiones.js
@@ -1,12 +1,39 @@
 var url = 'http://localhost:8000/'  
 
-const fetchUsers = () => {
+// Funciones a llamar en el fichero de leap
+/**
+ * Manda una petición 
+ * @param {*} data variable a enviar
+ * @param {*} route a qué ruta se le hace el put
+ */
+const putData = (data, route) => {
+    const custom_url = url + route + '/' + data
+    axios.put( custom_url) 
+        .then(response => {
+            const data = response.data;
+            console.log(`PUT to ${custom_url}`, data);
+        })
+}
+//--------------------
+// Funciones con las que testear. Tienen sentido dentro del fichero pruebasPut.html 
+// Es una petición de lectura de la web 
+const fetchDataAux = () => {
+// Cogemos los valores 
+var swipe = document.getElementById("swipe").value
+var dedos = document.getElementById("dedos").value
+var tap = document.getElementById('tap').value 
+// actualizamos el contenido, haciendo peticiones de put
+putData(swipe, 'swipe')
+putData(dedos, 'dedos')
+putData(tap, 'tap')
+//refrescamos el contenido de la página
 axios.get(url)
 .then(response => {
-    const users = response.data;
-    console.log(`GET list users`, users);
+    const data = response.data;
+    console.log(`GET data`, data);
+    // Recargamos el div que hay al final de pruebasPut.html
+    document.getElementById("mensaje").innerHTML = JSON.stringify(data);
 })
 .catch(error => console.error(error));
 };
 
-fetchUsers();
